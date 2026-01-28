bff35611402f2c64f1fe847557b651df2eb8b33c
Jason <gjasonuf@gmail.com>
2026-01-28 14:42:16 -0500

    revamped the beginning code and added the example in the assignment page as a template

--- Assignment1/taskA.py
+++ Assignment1/taskA.py
@@ -18,43 +18,72 @@ while (some hospital is free and hasn’t been matched/assigned
 }
 '''
 
+# Example input
+n = 3
+data = [
+    [1, 2, 3],  # hospital 1 prefs
+    [2, 3, 1],  # hospital 2 prefs
+    [2, 1, 3],  # hospital 3 prefs
+    [2, 1, 3],  # student 1 prefs
+    [1, 2, 3],  # student 2 prefs
+    [1, 2, 3]   # student 3 prefs
+]
+
 person = defaultdict(list)
 hospitals = defaultdict(list)
 
-#Initialized 
-person["Xavier"] = [["Shands",True], ["North", True], ["Vetrans",True]]
-person["Yancey"] = [["North",True], ["Shands", True], ["Vetrans",True]]
-person["Zeus"] = [["Shands",True], ["North", True], ["Vetrans",True]]
+index = 1
+for i in range(n):
+    hospitals[index] = [[s, True] for s in data[i]]
+    index += 1
 
-hospitals["Shands"] = [["Yancey",True], ["Xavier", True], ["Zeus",True]]
-hospitals["North"] = [["Shands",True], ["Yancey", True], ["Zeus",True]]
-hospitals["Vetrans"] = [["Xavier",True], ["Yancey", True], ["Zeus",True]]
+index = 1
+for i in range(n, 2 * n):
+    person[index] = [[h, True] for h in data[i]]
+    index += 1
+
+free = {}
+
+for i in range(n):
+    free[i + 1] = True
+
+
+# Initialized
+person["Xavier"] = [["Shands", True], ["North", True], ["Vetrans", True]]
+person["Yancey"] = [["North", True], ["Shands", True], ["Vetrans", True]]
+person["Zeus"] = [["Shands", True], ["North", True], ["Vetrans", True]]
+
+hospitals["Shands"] = [["Yancey", True], ["Xavier", True], ["Zeus", True]]
+hospitals["North"] = [["Shands", True], ["Yancey", True], ["Zeus", True]]
+hospitals["Vetrans"] = [["Xavier", True], ["Yancey", True], ["Zeus", True]]
 
 free = {
-    "Xavier" : True,
-    "Yancey" : True,
-    "Zeus" : True
+    "Xavier": True,
+    "Yancey": True,
+    "Zeus": True
 }
 
 applicants = deque(["Xavier", "Yancey", "Zeus"])
 unmatched_hospital = deque(["Shands", "North", "Vetrans"])
 
+
 def assign(h, a):
     pass
 
-def swap(h,a):
+
+def swap(h, a):
     pass
+
 
 perfer = dict()
 
 while unmatched_hospital:
 
     hospital = unmatched_hospital.popleft()
-    hospital_list = hospitals[hospital] #-> we get the list
+    hospital_list = hospitals[hospital]  # -> we get the list
     applicant = lol
-    #while True:
+    # while True:
     #    a = applicants.pop()
-
 
     if free[applicant]:
         assign(hospital, applicant)
@@ -62,6 +91,3 @@ while unmatched_hospital:
         swap(hospital, applicant)
     else:
         continue
-
-
-
