fa84ee523751deb4dea3bd59ef24f1571234eb95
JhonathanHerrera <herrerajhonathan@ufl.edu>
2026-01-28 14:38:02 -0500

    update

--- Assignment1/taskA.py
+++ Assignment1/taskA.py
@@ -16,8 +16,10 @@ while (some hospital is free and hasn’t been matched/assigned
     else
         a rejects h
 }
+
 '''
 
+'''
 person = defaultdict(list)
 hospitals = defaultdict(list)
 
@@ -38,29 +40,58 @@ free = {
 
 applicants = deque(["Xavier", "Yancey", "Zeus"])
 unmatched_hospital = deque(["Shands", "North", "Vetrans"])
+'''
+studentMatch = defaultdict(str)
+hosptialMatch = defaultdict(str)
+
+students = {
+        "Xavier" : ["Shands", "North","Vetrans"],
+        "Yancey" : ["North", "Shands", "Vetrans"],
+        "Zeus" : ["Shands", "North", "Vetrans"]
+    }
+hospitals = {
+    "Shands" : ["Yancey", "Xavier","Zeus"],
+     "North" : ["Xavier", "Yancey", "Zeus"],
+    "Vetrans" : ["Xavier", "Yancey", "Zeus"]
+}
+
+def initialize():
+    
+    for student in students:
+        studentMatch[student] = " "
+
+    for hosptial in hospitals:
+        hosptialMatch[hosptial] = " "
+
+initialize()
+
+#If the students have the ranking
+ranking = defaultdict(dict)
+
+for student, hospital_list in students.items():
+    ranking[student] = {v: i +1 for i,v in enumerate(hospital_list)}
 
 def assign(h, a):
-    pass
+    studentMatch[a] = h
+    hosptialMatch[h] = a
 
 def swap(h,a):
     pass
 
-perfer = dict()
+unmatched_hospital = deque(hospitals.keys())
 
 while unmatched_hospital:
 
     hospital = unmatched_hospital.popleft()
-    hospital_list = hospitals[hospital] #-> we get the list
-    applicant = lol
-    #while True:
-    #    a = applicants.pop()
+    
+    applicant = 1st applicant on h's list to whom hospital has not been matched
 
-
-    if free[applicant]:
+    if studentMatch[applicant] == " ":
         assign(hospital, applicant)
-    elif not free[applicant] and perfer[applicant] == hospital:
+    elif a prefers h to her/his current assignment h':
         swap(hospital, applicant)
     else:
+        #a rejects h 
         continue
 
 
