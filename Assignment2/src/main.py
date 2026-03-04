54c52dc9c3b960dafdc9c6a54de673722bccad47
Jason <gjasonuf@gmail.com>
2026-03-03 22:21:59 -0500

    added conditional check to see if k >= 1

--- Assignment2/src/main.py
+++ Assignment2/src/main.py
@@ -13,9 +13,12 @@ with open(sys.argv[1], "r") as f:
     r = [int(x) for x in f.readline().strip().split()] 
 
 
+if k >= 1:
     fifo = FIFO(k,m,r)
     fifo.fifo()
     lru = LRU(k,m,r)
     lru.lru()
     optff = OPTFF(k,m,r)
     optff.optff()
+else:
+    print("Invalid cache capacity. Cache capacity must be at least 1.")
