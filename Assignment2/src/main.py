bbc59ef853b0dcfc2ea3d39d0ceb2a5f2c77a306
Jason <gjasonuf@gmail.com>
2026-03-03 20:51:09 -0500

    fixed input parsing formatting

--- Assignment2/src/main.py
+++ Assignment2/src/main.py
@@ -8,7 +8,7 @@ from optff import OPTFF
 
 
 with open(sys.argv[1], "r") as f:
-    k ,m =  map(int, f.readline().split())
+    k, m =  map(int, f.readline().split())
 
     r = [int(x) for x in f.readline().strip().split()] 
 
